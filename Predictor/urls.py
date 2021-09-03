6ce71eb0934e0bdcb57209c23e6f072cf9cb67b1
ishikac167 <ishika.chokshi@svkmmumbai.onmicrosoft.com>
2021-09-03 14:30:42 +0530

    server run

--- Predictor/urls.py
+++ Predictor/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-# import views
+# from . import views
 
 # urlpatterns = [
-#     path('PCOD', views.PCOD, name='PCOD'),
+#     path('PCOD/', views.PCOD, name='PCOD'),
 #     path('result_PCOD/', views.result_PCOD, name='result_PCOD')
 # ]
