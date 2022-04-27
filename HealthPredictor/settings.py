afb50bbdfe31ff349a0f0c445dfc1ec3c9fe9105
Ishika Chokshi <ishika.chokshi@svkmmumbai.onmicrosoft.com>
2022-04-27 17:02:50 +0530

    profile

--- HealthPredictor/settings.py
+++ HealthPredictor/settings.py
@@ -145,3 +145,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'youremail@gmail.com'
 EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Predictor\generated.json"
